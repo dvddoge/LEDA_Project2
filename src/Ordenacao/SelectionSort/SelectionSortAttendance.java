c0fe1e3bd3a649a758390f77f5c1de96d9c0a79b
Deyvid Jerônimo <95938301+dvddoge@users.noreply.github.com>
2023-12-03 19:47:36 -0300

    Update SelectionSortAttendance.java

--- src/Ordenacao/SelectionSort/SelectionSortAttendance.java
+++ src/Ordenacao/SelectionSort/SelectionSortAttendance.java
@@ -1,9 +1,6 @@
 package Ordenacao.SelectionSort;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
@@ -12,8 +9,9 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * A classe {@code InsertionSortAttendance} realiza a ordenação de dados em
- * arquivos CSV usando o algoritmo de ordenação Insertion Sort.
+ * A classe {@code SelectionSortAttendance} realiza a ordenação de dados em
+ * arquivos CSV usando o algoritmo de ordenação Selection Sort para ordenação com
+ * base na coluna "attendance".
  * Ela é projetada para criar três casos de ordenação (melhor, médio e pior) e
  * medir o tempo de execução.
  * Os resultados ordenados são escritos nos arquivos de saída correspondentes.
@@ -25,10 +23,9 @@ public class SelectionSortAttendance {
     private String outputMedio = path + "matches_t2_attendance_selectionSort_medioCaso.csv";
     private String outputMelhor = path + "matches_t2_attendance_selectionSort_melhorCaso.csv";
     private String outputPior = path + "matches_t2_attendance_selectionSort_piorCaso.csv";
-    private int attendanceIndex = 6;
 
     /**
-     * Cria uma nova instância de {@code InsertionSortAttendance} com o arquivo de
+     * Cria uma nova instância de {@code SelectionSortAttendance} com o arquivo de
      * entrada especificado.
      *
      * @param inputFile O arquivo de entrada a ser ordenado.
@@ -66,53 +63,23 @@ public class SelectionSortAttendance {
 
     /**
      * Cria o caso de ordenação melhor ordenando o arquivo de entrada de forma
-     * crescente.
+     * crescente usando o algoritmo Selection Sort.
      */
     private void criarCasoMelhor() {
-        try {
-            int rowCount = contarLinhas(inputFile);
-
-            String[][] data = carregarArquivoEmArray(inputFile, rowCount);
-            List<String[]> dataList = new ArrayList<>(List.of(data));
-
-            selectionSort(dataList, attendanceIndex);
-
-            // Escrevendo no arquivo de saída
-            try (FileWriter writer = new FileWriter(outputMelhor)) {
-                for (String[] row : dataList) {
-                    writer.write(String.join(",", row) + "\n");
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        List<String[]> data = carregarArquivoEmLista(inputFile);
+        selectionSort(data, 6); // 6 é o índice da coluna "attendance"
+        escreverDados(data, outputMelhor);
     }
 
     /**
      * Cria o caso de ordenação pior ordenando o arquivo de entrada de forma
-     * decrescente.
+     * decrescente usando o algoritmo Selection Sort.
      */
     private void criarCasoPior() {
-        try {
-            int rowCount = contarLinhas(inputFile);
-
-            String[][] data = carregarArquivoEmArray(inputFile, rowCount);
-            List<String[]> dataList = new ArrayList<>(List.of(data));
-
-            selectionSort(dataList, attendanceIndex);
-
-            // Invertendo a ordem dos dados
-            Collections.reverse(dataList);
-
-            // Escrevendo no arquivo de saída
-            try (FileWriter writer = new FileWriter(outputPior)) {
-                for (String[] row : dataList) {
-                    writer.write(String.join(",", row) + "\n");
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        List<String[]> data = carregarArquivoEmLista(inputFile);
+        selectionSort(data, 6); // 6 é o índice da coluna "attendance"
+        Collections.reverse(data); // Inverte a lista
+        escreverDados(data, outputPior);
     }
 
     /**
@@ -123,7 +90,7 @@ public class SelectionSortAttendance {
      */
     private void copiarArquivo(String origem, String destino) {
         try (BufferedReader br = new BufferedReader(new FileReader(origem));
-                FileWriter writer = new FileWriter(destino)) {
+             FileWriter writer = new FileWriter(destino)) {
             String line;
             while ((line = br.readLine()) != null) {
                 writer.write(line + "\n");
@@ -134,82 +101,95 @@ public class SelectionSortAttendance {
     }
 
     /**
-     * Conta o número de linhas em um arquivo.
+     * Carrega os dados de um arquivo CSV em uma lista de arrays de strings.
      *
-     * @param fileToOrder O arquivo para o qual as linhas serão contadas.
-     * @return O número de linhas no arquivo.
-     * @throws IOException Se ocorrer um erro de leitura do arquivo.
+     * @param file O arquivo CSV a ser carregado.
+     * @return Uma lista de arrays de strings contendo os dados do arquivo.
      */
-    private int contarLinhas(String fileToOrder) throws IOException {
-        int rowCount = 0;
-        try (BufferedReader counter = new BufferedReader(new FileReader(fileToOrder))) {
-            while (counter.readLine() != null)
-                rowCount++;
-        }
-        return rowCount;
-    }
-
-    /**
-     * Carrega os dados de um arquivo CSV em um array bidimensional.
-     *
-     * @param fileToOrder O arquivo CSV a ser carregado.
-     * @param rowCount    O número de linhas no arquivo.
-     * @return Um array bidimensional contendo os dados do arquivo.
-     * @throws IOException Se ocorrer um erro de leitura do arquivo.
-     */
-    private String[][] carregarArquivoEmArray(String fileToOrder, int rowCount) throws IOException {
-        String[][] data = new String[rowCount][14];
-
-        int index = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader(fileToOrder))) {
+    private List<String[]> carregarArquivoEmLista(String file) {
+        List<String[]> data = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
-                data[index++] = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                data.add(values);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return data;
     }
 
     /**
-     * Ordena um array bidimensional com base em uma coluna específica usando o
-     * algoritmo Selection Sort.
+     * Escreve os dados de uma lista de arrays de strings em um arquivo CSV.
      *
-     * @param data        O array bidimensional a ser ordenado.
-     * @param columnIndex O índice da coluna pela qual os dados serão ordenados.
-     * @param rowCount    O número de linhas no array.
+     * @param data       A lista de arrays de strings contendo os dados a serem escritos.
+     * @param outputFile O arquivo CSV de saída.
      */
-    private void selectionSort(List<String[]> data, int columnIndex) {
-        for (int i = 0; i < data.size() - 1; i++) {
-            int min_idx = i;
-            for (int j = i + 1; j < data.size(); j++) {
-                String currentData = data.get(j)[columnIndex].replace("\"", "").replace(",", "");
-                String minData = data.get(min_idx)[columnIndex].replace("\"", "").replace(",", "");
+    private void escreverDados(List<String[]> data, String outputFile) {
+        try (FileWriter writer = new FileWriter(outputFile)) {
+            if (outputFile != outputMelhor)
+                writer.write(
+                        "id,home,away,date,year,time (utc),attendance,venue,league,home_score,away_score,home_goal_scorers,away_goal_scorers,full_date\n");
 
-                if (!isNumeric(currentData)) {
-                    currentData = "0";
-                }
-                if (!isNumeric(minData)) {
-                    minData = "0";
-                }
-
-                int currentVal = Integer.parseInt(currentData);
-                int minVal = Integer.parseInt(minData);
-
-                if (currentVal < minVal) {
-                    min_idx = j;
-                }
+            // Escreva os dados
+            for (String[] values : data) {
+                writer.write(String.join(",", values) + "\n");
             }
-
-            Collections.swap(data, min_idx, i);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     /**
+     * Realiza a ordenação usando o algoritmo Selection Sort.
+     *
+     * @param data       A lista de arrays de strings contendo os dados a serem ordenados.
+     * @param columnIndex O índice da coluna pela qual a ordenação será feita.
+     */
+    private void selectionSort(List<String[]> data, int columnIndex) {
+        int n = data.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                int currentValue = parseToInt(data.get(j)[columnIndex]);
+                int minValue = parseToInt(data.get(minIndex)[columnIndex]);
+
+                if (currentValue < minValue) {
+                    minIndex = j;
+                }
+            }
+
+            // Trocar os elementos
+            Collections.swap(data, i, minIndex);
+        }
+    }
+
+    /**
+     * Converte uma string em um número inteiro.
+     * Se a string estiver vazia ou não for numérica, retorna 0.
+     * O valor pode ter aspas duplas e vírgulas, que são removidas antes da
+     * conversão.
+     *
+     * @param value A string a ser convertida em um número inteiro.
+     * @return O valor inteiro correspondente à string ou 0 se a string não for
+     * numérica.
+     */
+    private int parseToInt(String value) {
+        value = value.replace("\"", "").replace(",", "");
+        if (value.isEmpty() || !isNumeric(value)) {
+            return 0;
+        }
+        return Integer.parseInt(value);
+    }
+
+    /**
      * Verifica se uma string pode ser convertida em um número inteiro.
+     * Esta função é usada para garantir que tentativas de conversão sejam seguras.
      *
      * @param str A string a ser verificada.
      * @return {@code true} se a string for um número inteiro válido, caso contrário
-     *         {@code false}.
+     * {@code false}.
      */
     private boolean isNumeric(String str) {
         try {
@@ -220,26 +200,9 @@ public class SelectionSortAttendance {
         }
     }
 
-    private void ordenarEImprimirTempo(String fileToOrder) {
-        try {
-            int rowCount = contarLinhas(fileToOrder);
-            String[][] data = carregarArquivoEmArray(fileToOrder, rowCount);
-            List<String[]> dataList = new ArrayList<>(List.of(data));
-
-            long startTime = System.currentTimeMillis();
-
-            selectionSort(dataList, attendanceIndex);
-
-            long endTime = System.currentTimeMillis();
-            System.out.println("Tempo de execução para " + fileToOrder + ": " + (endTime - startTime) + " ms");
-            imprimirConsumoMemoria();
-
-            // NÃO gravaremos os dados ordenados no arquivo para manter os casos como estão
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+    /**
+     * Imprime o consumo de memória atual.
+     */
     private void imprimirConsumoMemoria() {
         MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
         MemoryUsage heapMemoryUsage = memoryBean.getHeapMemoryUsage();
@@ -248,5 +211,21 @@ public class SelectionSortAttendance {
 
         System.out.println("Consumo de memória: " + usedMemory + " bytes");
     }
-}
 
+    /**
+     * Realiza a ordenação e mede o tempo de execução usando o algoritmo Selection Sort.
+     * O tempo de execução é impresso no console.
+     *
+     * @param fileToOrder O arquivo a ser ordenado.
+     */
+    private void ordenarEImprimirTempo(String fileToOrder) {
+        List<String[]> data = carregarArquivoEmLista(fileToOrder);
+
+        long startTime = System.currentTimeMillis();
+        selectionSort(data, 6); // 6 é o índice da coluna "attendance"
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Tempo de execução para " + fileToOrder + ": " + (endTime - startTime) + " ms");
+        imprimirConsumoMemoria(); // Imprimir consumo de memória após a ordenação
+    }
+}
