af8a65eedd615803b6e1a7a543feda3f5d166005
Deyvid Jerônimo <95938301+dvddoge@users.noreply.github.com>
2023-12-03 20:07:43 -0300

    Update SelectionSortFullDate.java

--- src/Ordenacao/SelectionSort/SelectionSortFullDate.java
+++ src/Ordenacao/SelectionSort/SelectionSortFullDate.java
@@ -1,10 +1,6 @@
 package Ordenacao.SelectionSort;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -16,12 +12,8 @@ import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
 
 /**
- * A classe {@code SelectionSortFullDate} realiza a ordenação de dados em
- * arquivos
- * CSV usando o algoritmo de ordenação Selection Sort.
- * Ela é projetada para criar três casos de ordenação (melhor, médio e pior) e
- * medir o tempo de execução.
- * Os resultados ordenados são escritos nos arquivos de saída correspondentes.
+ * Classe que realiza a ordenação de dados em arquivos CSV usando o algoritmo de
+ * ordenação Selection Sort para ordenação com base na coluna "full_date".
  */
 public class SelectionSortFullDate {
 
@@ -34,18 +26,17 @@ public class SelectionSortFullDate {
     private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     /**
-     * Construtor da classe SelectionSortFullDate.
+     * Construtor que inicializa a classe com o arquivo de entrada fornecido.
      *
-     * @param inputFile O caminho do arquivo de entrada contendo os dados não
-     *                  ordenados.
+     * @param inputFile O arquivo de entrada contendo os dados a serem ordenados.
      */
     public SelectionSortFullDate(String inputFile) {
         this.inputFile = inputFile;
     }
 
     /**
-     * Método principal que realiza a ordenação em diferentes cenários (melhor,
-     * médio e pior caso).
+     * Realiza a ordenação dos dados nos casos de melhor, médio e pior e imprime os
+     * tempos de execução.
      */
     public void ordenar() {
         criarCasoMelhor();
@@ -63,47 +54,43 @@ public class SelectionSortFullDate {
     }
 
     /**
-     * Cria o cenário de caso médio, copiando o arquivo de entrada para um novo
-     * arquivo de saída.
+     * Cria o caso de ordenação médio copiando o conteúdo do arquivo de entrada para
+     * o arquivo de saída.
      */
     private void criarCasoMedio() {
         copiarArquivo(inputFile, outputMedio);
     }
 
     /**
-     * Cria o cenário de caso melhor, ordenando os dados em ordem crescente de
-     * datas.
+     * Cria o caso de ordenação melhor ordenando o arquivo de entrada usando o
+     * algoritmo Selection Sort.
      */
     private void criarCasoMelhor() {
-        String[][] data = carregarArquivoEmArray(inputFile);
-        List<String[]> dataList = new ArrayList<>(List.of(data));
-
-        selectionSort(dataList, fullDateIndex);
-        escreverDados(dataList, outputMelhor);
+        List<String[]> data = carregarArquivoEmArrayList(inputFile);
+        selectionSort(data, fullDateIndex);
+        escreverDados(data, outputMelhor);
     }
 
     /**
-     * Cria o cenário de caso pior, ordenando os dados em ordem decrescente de
-     * datas.
+     * Cria o caso de ordenação pior ordenando o arquivo de entrada usando o
+     * algoritmo Selection Sort e depois invertendo a ordem dos dados.
      */
     private void criarCasoPior() {
-        String[][] data = carregarArquivoEmArray(inputFile);
-        List<String[]> dataList = new ArrayList<>(List.of(data));
-
-        selectionSort(dataList, fullDateIndex);
-        inverterDados(dataList);
-        escreverDados(dataList, outputPior);
+        List<String[]> data = carregarArquivoEmArrayList(inputFile);
+        selectionSort(data, fullDateIndex);
+        Collections.reverse(data);
+        escreverDados(data, outputPior);
     }
 
     /**
      * Copia um arquivo de origem para um arquivo de destino.
      *
-     * @param origem  O caminho do arquivo de origem.
-     * @param destino O caminho do arquivo de destino.
+     * @param origem  O arquivo de origem a ser copiado.
+     * @param destino O arquivo de destino onde o conteúdo será copiado.
      */
     private void copiarArquivo(String origem, String destino) {
         try (BufferedReader br = new BufferedReader(new FileReader(origem));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(destino))) {
+             BufferedWriter writer = new BufferedWriter(new FileWriter(destino))) {
             String line;
             while ((line = br.readLine()) != null) {
                 writer.write(line);
@@ -115,28 +102,36 @@ public class SelectionSortFullDate {
     }
 
     /**
-     * Carrega os dados de um arquivo CSV em uma matriz bidimensional.
+     * Carrega os dados de um arquivo CSV em um ArrayList de arrays de strings.
      *
-     * @param file O caminho do arquivo CSV.
-     * @return Uma matriz bidimensional contendo os dados do arquivo.
+     * @param file O arquivo CSV a ser carregado.
+     * @return Um ArrayList contendo os dados do arquivo, onde cada elemento é um
+     *         array de strings representando uma linha do arquivo.
      */
-    private String[][] carregarArquivoEmArray(String file) {
-        String[][] data;
+    private List<String[]> carregarArquivoEmArrayList(String file) {
+        List<String[]> data = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            data = br.lines().skip(1).map(line -> line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1))
-                    .toArray(String[][]::new);
+            String line;
+            boolean firstLine = true;
+            while ((line = br.readLine()) != null) {
+                if (firstLine) {
+                    firstLine = false;
+                    continue; // Pular o cabeçalho
+                }
+                String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                data.add(values);
+            }
         } catch (IOException e) {
             e.printStackTrace();
-            data = new String[0][];
         }
         return data;
     }
 
     /**
-     * Escreve os dados de uma matriz bidimensional em um arquivo CSV.
+     * Escreve os dados de uma lista de arrays de strings em um arquivo CSV.
      *
-     * @param data       A matriz bidimensional de dados a ser escrita.
-     * @param outputFile O caminho do arquivo CSV de saída.
+     * @param data       A lista de arrays de strings contendo os dados.
+     * @param outputFile O arquivo onde os dados serão escritos.
      */
     private void escreverDados(List<String[]> data, String outputFile) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
@@ -156,98 +151,68 @@ public class SelectionSortFullDate {
     }
 
     /**
-     * Inverte os dados em uma matriz bidimensional.
+     * Ordena os dados na lista usando o algoritmo Selection Sort.
      *
-     * @param data A matriz bidimensional de dados a ser invertida.
-     */
-    private void inverterDados(List<String[]> data) {
-        Collections.reverse(data);
-    }
-
-    /**
-     * Imprime o tempo de execução da ordenação de um arquivo especificado.
-     *
-     * @param fileToOrder O caminho do arquivo contendo os dados a serem ordenados.
-     */
-    private void ordenarEImprimirTempo(String fileToOrder) {
-        List<String[]> dataList = carregarArquivoEmLista(fileToOrder);
-    
-        long startTime = System.currentTimeMillis();
-        selectionSort(dataList, fullDateIndex);
-        long endTime = System.currentTimeMillis();
-    
-        System.out.println("Tempo de execução para " + fileToOrder + ": " + (endTime - startTime) + " ms");
-        imprimirConsumoMemoria(); // Imprimir consumo de memória após a ordenação
-    }
-    
-    private List<String[]> carregarArquivoEmLista(String fileToOrder) {
-        List<String[]> data = new ArrayList<>();
-    
-        try (BufferedReader br = new BufferedReader(new FileReader(fileToOrder))) {
-            String line;
-            boolean firstLine = true;
-            while ((line = br.readLine()) != null) {
-                if (firstLine) {
-                    firstLine = false;
-                    continue;  // Pular a primeira linha (cabeçalho)
-                }
-                data.add(line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    
-        return data;
-    }
-    
-
-    /**
-     * Ordena um array bidimensional com base em uma coluna específica usando o
-     * algoritmo Selection Sort.
-     *
-     * @param data        O array bidimensional a ser ordenado.
+     * @param data        A lista de arrays de strings a ser ordenada.
      * @param columnIndex O índice da coluna pela qual os dados serão ordenados.
      */
     private void selectionSort(List<String[]> data, int columnIndex) {
         int n = data.size();
-
         for (int i = 0; i < n - 1; i++) {
-            int min_idx = i;
-
+            int minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if (isDateGreater(data.get(min_idx)[columnIndex], data.get(j)[columnIndex])) {
-                    min_idx = j;
+                int compareResult = compareDates(data.get(j)[columnIndex], data.get(minIndex)[columnIndex]);
+                if (compareResult < 0) {
+                    minIndex = j;
                 }
             }
-
-            Collections.swap(data, min_idx, i);
+            Collections.swap(data, i, minIndex);
         }
     }
 
     /**
-     * Verifica se uma data é maior que outra.
+     * Compara duas datas no formato "dd/MM/yyyy".
      *
      * @param date1 A primeira data a ser comparada.
      * @param date2 A segunda data a ser comparada.
-     * @return true se date1 for maior que date2, caso contrário, false.
+     * @return Um valor negativo se date1 < date2, um valor positivo se date1 > date2,
+     *         ou zero se date1 == date2.
      */
-    private boolean isDateGreater(String date1, String date2) {
+    private int compareDates(String date1, String date2) {
         try {
-            Date d1 = sdf.parse(date1.replace("\"", ""));
-            Date d2 = sdf.parse(date2.replace("\"", ""));
-            return d1.compareTo(d2) > 0;
+            Date d1 = sdf.parse(date1.replace("\"", "").trim());
+            Date d2 = sdf.parse(date2.replace("\"", "").trim());
+            return d1.compareTo(d2);
         } catch (ParseException e) {
             e.printStackTrace();
-            return false;
+            return 0;
         }
     }
 
+    /**
+     * Imprime o consumo de memória após a ordenação.
+     */
     private void imprimirConsumoMemoria() {
         MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
         MemoryUsage heapMemoryUsage = memoryBean.getHeapMemoryUsage();
-
         long usedMemory = heapMemoryUsage.getUsed();
-
         System.out.println("Consumo de memória: " + usedMemory + " bytes");
+    }
+
+    /**
+     * Realiza a ordenação e mede o tempo de execução usando o algoritmo Selection
+     * Sort. O tempo de execução é impresso no console.
+     *
+     * @param fileToOrder O caminho do arquivo a ser ordenado e medido.
+     */
+    private void ordenarEImprimirTempo(String fileToOrder) {
+        List<String[]> data = carregarArquivoEmArrayList(fileToOrder);
+
+        long startTime = System.currentTimeMillis();
+        selectionSort(data, fullDateIndex);
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Tempo de execução para " + fileToOrder + ": " + (endTime - startTime) + " ms");
+        imprimirConsumoMemoria(); // Imprimir consumo de memória após a ordenação
     }
 }
